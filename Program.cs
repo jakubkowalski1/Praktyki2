82be0edc1cd9f7f3579806d276c8adcfb84e43f8
Malutki Kubuś <jakub.kowalski1029@gmail.com>
2024-06-07 01:01:18 +0200

    nic nie dziala

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using ProjektPraktyki_2._0.Models;
 
 
 
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.\
@@ -32,3 +31,43 @@ app.MapControllers();
 
 app.Run();
 
+var appBuilder = WebApplication.CreateBuilder(args);
+
+// Dodaj us³ugi do kontenera.
+appBuilder.Services.AddControllersWithViews();
+
+var bApp = builder.Build();
+
+// Skonfiguruj potok HTTP.
+if (bApp.Environment.IsDevelopment())
+{
+    bApp.UseDeveloperExceptionPage();
+}
+else
+{
+    bApp.UseExceptionHandler("/Home/Error");
+    bApp.UseHsts();
+}
+
+bApp.UseHttpsRedirection();
+bApp.UseStaticFiles();
+
+bApp.UseRouting();
+
+bApp.UseAuthorization();
+
+bApp.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.UseSpa(spa =>
+{
+    spa.Options.SourcePath = "ClientApp";
+
+    if (bApp.Environment.IsDevelopment())
+    {
+        spa.UseProxyToSpaDevelopmentServer("http://localhost:3000");
+    }
+});
+
+bApp.Run();
